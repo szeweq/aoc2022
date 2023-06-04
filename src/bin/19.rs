a7df0a244c31daf799b0764bd3796ad70c34c59a
Szewek <shevkpl@gmail.com>
2023-06-04 13:23:18 +0200

    [Day 19] Use struct and u16 for better performance

--- src/bin/19.rs
+++ src/bin/19.rs
@@ -4,16 +4,16 @@
  */
 use std::collections::{VecDeque, HashSet};
 
-fn parse_line(line: &str) -> [u32; 6] {
+fn parse_line(line: &str) -> [u16; 6] {
     let mut sp = line.split(" Each ");
     sp.next().unwrap();
-    let oc = (sp.next().unwrap().as_bytes()[16] - b'0') as u32;
-    let cc = (sp.next().unwrap().as_bytes()[17] - b'0') as u32;
+    let oc = (sp.next().unwrap().as_bytes()[16] - b'0') as u16;
+    let cc = (sp.next().unwrap().as_bytes()[17] - b'0') as u16;
     let obs = sp.next().unwrap();
-    let boc = (obs.as_bytes()[21] - b'0') as u32;
+    let boc = (obs.as_bytes()[21] - b'0') as u16;
     let bcc = obs[31..].split_once(' ').unwrap().0.parse().unwrap();
     let geode = sp.next().unwrap();
-    let goc = (geode.as_bytes()[18] - b'0') as u32;
+    let goc = (geode.as_bytes()[18] - b'0') as u16;
     let gbc = geode[28..].split_once(' ').unwrap().0.parse().unwrap();
     [oc, cc, boc, bcc, goc, gbc]
 }
@@ -26,103 +26,100 @@ macro_rules! set_min {
     };
 }
 
-const M_ORE: usize = 0;
-const M_CLAY: usize = 1;
-const M_OBS: usize = 2;
-const M_GEODE: usize = 3;
+#[derive(Clone, PartialEq, Eq, Hash)]
+struct MineState { // m_ = miners, r_ = resources
+    m_ore: u16,
+    m_clay: u16,
+    m_obs: u16,
+    m_geode: u16,
+    r_ore: u16,
+    r_clay: u16,
+    r_obs: u16,
+    r_geode: u16
+}
 
-fn sim_blueprint(time: u32, costs: [u32; 6]) -> u32 {
+fn sim_blueprint(time: u16, costs: [u16; 6]) -> u16 {
     let [c_ore, c_clay, c_obs_o, c_obs_c, c_geo_o, c_geo_b] = costs;
-    let state: [[u32; 4]; 2] = [[1, 0, 0, 0], [0, 0, 0, 0]];
+    let state = MineState {m_ore: 1, m_clay: 0, m_obs: 0, m_geode: 0, r_ore: 0, r_clay: 0, r_obs: 0, r_geode: 0};
     let ototal = c_ore.max(c_clay).max(c_obs_o).max(c_geo_o);
     let mut vq = VecDeque::from([(state, time)]);
     let mut hs = HashSet::new();
     let mut max_geode = 0;
     let mut lt = time;
-    let mut update = Vec::new();
-    while let Some((cs, t)) = vq.pop_front() {
-        let [mut miners, mut mats] = cs;
-        
+    while let Some((mut cs, t)) = vq.pop_front() {
         if t == 0 {
-            if max_geode < mats[M_GEODE] {
-                max_geode = mats[M_GEODE];
+            if max_geode < cs.r_geode {
+                max_geode = cs.r_geode;
             }
             continue;
         }
 
-        set_min!(miners[M_ORE], ototal);
-        set_min!(miners[M_CLAY], c_obs_c);
-        set_min!(miners[M_OBS], c_geo_b);
+        set_min!(cs.m_ore, ototal);
+        set_min!(cs.m_clay, c_obs_c);
+        set_min!(cs.m_obs, c_geo_b);
 
         
         let nt = t - 1;
-        let mine_ore = t * ototal - miners[M_ORE] * nt;
-        set_min!(mats[M_ORE], mine_ore);
-        let mine_clay = t * c_obs_c - miners[M_CLAY] * nt;
-        set_min!(mats[M_CLAY], mine_clay);
-        let mine_obs = t * c_geo_b - miners[M_OBS] * nt;
-        set_min!(mats[M_OBS], mine_obs);
+        let mine_ore = t * ototal - cs.m_ore * nt;
+        set_min!(cs.r_ore, mine_ore);
+        let mine_clay = t * c_obs_c - cs.m_clay * nt;
+        set_min!(cs.r_clay, mine_clay);
+        let mine_obs = t * c_geo_b - cs.m_obs * nt;
+        set_min!(cs.r_obs, mine_obs);
 
         if lt > t {
             hs.clear();
             lt = t;
-        } else if lt == t && !hs.insert([miners, mats]) {
+        } else if lt == t && !hs.insert(cs.clone()) {
             continue;
         }
 
-        let [lco, lcc, lcb, _] = mats;
+        let mut ncs = cs.clone();
+        ncs.r_ore += ncs.m_ore;
+        ncs.r_clay += ncs.m_clay;
+        ncs.r_obs += ncs.m_obs;
+        ncs.r_geode += ncs.m_geode;
 
-        mats[M_ORE] += miners[M_ORE];
-        mats[M_CLAY] += miners[M_CLAY];
-        mats[M_OBS] += miners[M_OBS];
-        mats[M_GEODE] += miners[M_GEODE];
-
-        update.push(([miners, mats], nt));
-        if lco >= c_ore {
-            let mut nminers = miners.clone();
-            let mut nmats = mats.clone();
-            nminers[M_ORE] += 1;
-            nmats[M_ORE] -= c_ore;
-            update.push(([nminers, nmats], nt));
+        vq.push_back((ncs.clone(), nt));
+        if cs.r_ore >= c_ore {
+            let mut vcs = ncs.clone();
+            vcs.m_ore += 1;
+            vcs.r_ore -= c_ore;
+            vq.push_back((vcs, nt));
         }
-        if lco >= c_clay {
-            let mut nminers = miners.clone();
-            let mut nmats = mats.clone();
-            nminers[M_CLAY] += 1;
-            nmats[M_ORE] -= c_clay;
-            update.push(([nminers, nmats], nt));
+        if cs.r_ore >= c_clay {
+            let mut vcs = ncs.clone();
+            vcs.m_clay += 1;
+            vcs.r_ore -= c_clay;
+            vq.push_back((vcs, nt));
         }
-        if lco >= c_obs_o && lcc >= c_obs_c {
-            let mut nminers = miners.clone();
-            let mut nmats = mats.clone();
-            nminers[M_OBS] += 1;
-            nmats[M_ORE] -= c_obs_o;
-            nmats[M_CLAY] -= c_obs_c;
-            update.push(([nminers, nmats], nt));
+        if cs.r_ore >= c_obs_o && cs.r_clay >= c_obs_c {
+            let mut vcs = ncs.clone();
+            vcs.m_obs += 1;
+            vcs.r_ore -= c_obs_o;
+            vcs.r_clay -= c_obs_c;
+            vq.push_back((vcs, nt));
         }
-        if lco >= c_geo_o && lcb >= c_geo_b {
-            let mut nminers = miners.clone();
-            let mut nmats = mats.clone();
-            nminers[M_GEODE] += 1;
-            nmats[M_ORE] -= c_geo_o;
-            nmats[M_OBS] -= c_geo_b;
-            update.push(([nminers, nmats], nt));
+        if cs.r_ore >= c_geo_o && cs.r_obs >= c_geo_b {
+            let mut vcs = ncs.clone();
+            vcs.m_geode += 1;
+            vcs.r_ore -= c_geo_o;
+            vcs.r_obs -= c_geo_b;
+            vq.push_back((vcs, nt));
         }
-        vq.extend(&update);
-        update.clear();
     }
     max_geode
 }
 
-pub fn part_1(input: &str) -> Option<u32> {
+pub fn part_1(input: &str) -> Option<u16> {
     let bpv = input.lines().map(parse_line).enumerate().collect::<Vec<_>>();
     Some(bpv.into_iter()
-        .map(|(i, c)| (i as u32 + 1) * sim_blueprint(24, c))
+        .map(|(i, c)| (i as u16 + 1) * sim_blueprint(24, c))
         .sum()
     )
 }
 
-pub fn part_2(input: &str) -> Option<u32> {
+pub fn part_2(input: &str) -> Option<u16> {
     let bpv = input.lines().take(3).map(parse_line).collect::<Vec<_>>();
     Some(bpv.into_iter()
         .map(|c| sim_blueprint(32, c))
