0b6a1ad15620f135a06e73e5f045550b492575bd
Szewek <shevkpl@gmail.com>
2023-06-04 22:49:13 +0200

    [Day 21] Use MTree struct

--- src/bin/21.rs
+++ src/bin/21.rs
@@ -47,6 +47,125 @@ macro_rules! mn {
 const M_ROOT: Mn = u32::from_ne_bytes(*b"root");
 const M_HUMN: Mn = u32::from_ne_bytes(*b"humn");
 
+struct MTree(HashMap<Mn, OpType>);
+impl MTree {
+    fn new(input: &str) -> Self {
+        Self (input.lines().map(parse_line).collect())
+    }
+    fn calc(&mut self, start: Mn) -> Option<i64> {
+        use OpType::*;
+        let mut q = vec![start];
+        while let Some(cm) = q.last() {
+            if let O(a, op, b) = &self.0[cm] {
+                let av = self.0[a];
+                let bv = self.0[b];
+                if let (N(na), N(nb)) = (av, bv) {
+                    let nn = op.do_op(na, nb);
+                    self.0.insert(*cm, N(nn));
+                    q.pop();
+                    continue;
+                }
+                if is_op!(av) {
+                    q.push(*a);
+                }
+                if is_op!(bv) {
+                    q.push(*b);
+                }
+            }
+        }
+        if let OpType::N(x) = self.0[&start] { Some(x) } else { None }
+    }
+
+    /// This optimization algorithm assumes that each monkey yells to another one.
+    fn optimize(&mut self, start: Mn) -> Option<Mn> {
+        use OpType::*;
+        let mut q = vec![start];
+        let mut hs = HashSet::new();
+        let mut hv = None;
+        while let Some(cm) = q.pop() {
+            if let O(a, op, b) = self.0[&cm] {
+                let av = self.0[&a];
+                let bv = self.0[&b];
+                if a == M_HUMN || b == M_HUMN {
+                    hv = Some(q[1])
+                }
+                if a != M_HUMN && b != M_HUMN {
+                    if let (N(na), N(nb)) = (&av, &bv) {
+                        if matches!(op, Op::Mul) && (na > &20 || nb > &20) {
+                            continue;
+                        }
+                        self.0.remove(&a);
+                        self.0.remove(&b);
+                        let nn = op.do_op(*na, *nb);
+                        self.0.insert(cm, N(nn));
+                        continue;
+                    }
+                }
+                if hs.insert(cm) {
+                    q.push(cm);
+                    if is_op!(av) {
+                        q.push(a);
+                    }
+                    if is_op!(bv) {
+                        q.push(b);
+                    }
+                }
+            }
+        }
+        self.0.shrink_to_fit();
+        hv
+    }
+
+    fn resolve(&mut self, start: Mn, rv: i64) -> Option<i64> {
+        use OpType::*;
+        let mut q = vec![start];
+        let mut opv = vec![];
+        while let Some(cm) = q.pop() {
+            let cv = self.0[&cm];
+            if let O(a, op, b) = cv {
+                let av = self.0[&a];
+                let bv = self.0[&b];
+                if a == M_HUMN || b == M_HUMN {
+                    let N(on) = (if a == M_HUMN { bv } else { av }) else { panic!("Not optimized!") };
+                    opv.push((op, on));
+                } else if opv.len() > 0 {
+                    let on = match (av, bv) {
+                        (N(_), N(_)) | (O(_, _, _), O(_, _, _)) => panic!("Not optimized!"),
+                        (N(x), O(_, _, _)) => x,
+                        (O(_, _, _), N(x)) => x
+                    };
+                    opv.push((op, on));
+                } else {
+                    q.push(cm);
+                    if is_op!(av) {
+                        q.push(a);
+                    }
+                    if is_op!(bv) {
+                        q.push(b);
+                    }
+                }
+            }
+        }
+        let mut uhs = HashSet::new();
+        uhs.insert(rv);
+        while let Some((o, n)) = opv.pop() {
+            let cuv = uhs.drain().collect::<Vec<_>>();
+            for v in cuv {
+                match o {
+                    Op::Div => {
+                        let mv = v * n;
+                        uhs.extend(mv..mv+n);
+                    }
+                    Op::Add => { uhs.insert(v - n); }
+                    Op::Sub => { uhs.insert(v + n); }
+                    Op::Mul => { uhs.insert(v / n); }
+                }
+            }
+        }
+        uhs.into_iter().min()
+    }
+}
+
 fn parse_line(l: &str) -> (Mn, OpType) {
     let bl = l.as_bytes();
     let r: Mn = mn![bl];
@@ -66,150 +185,32 @@ fn parse_line(l: &str) -> (Mn, OpType) {
     (r, op)
 }
 
-fn calculate(map: &mut HashMap<Mn, OpType>, queue: &mut Vec<Mn>, start: Mn) -> Option<i64> {
-    use OpType::*;
-    queue.push(start);
-    while let Some(cm) = queue.last() {
-        if let O(a, op, b) = &map[cm] {
-            let av = map[a];
-            let bv = map[b];
-            if let (N(na), N(nb)) = (av, bv) {
-                let nn = op.do_op(na, nb);
-                map.insert(*cm, N(nn));
-                queue.pop();
-                continue;
-            }
-            if is_op!(av) {
-                queue.push(*a);
-            }
-            if is_op!(bv) {
-                queue.push(*b);
-            }
-        }
-    }
-    if let OpType::N(x) = map[&start] { Some(x) } else { None }
-}
-
 pub fn part_1(input: &str) -> Option<i64> {
-    let mut m = input.lines().map(parse_line).collect::<HashMap<_, _>>();
-    let mut q: Vec<Mn> = Vec::new();
-    calculate(&mut m, &mut q, M_ROOT)
+    MTree::new(input).calc(M_ROOT)
 }
 
-/// This optimization algorithm assumes that each monkey yells to another one.
-fn optimize(map: &mut HashMap<Mn, OpType>, queue: &mut Vec<Mn>, start: Mn) -> Option<Mn> {
-    use OpType::*;
-    let mut hs = HashSet::new();
-    let mut hv = None;
-    queue.push(start);
-    while let Some(cm) = queue.pop() {
-        let cv = map[&cm];
-        if let O(a, op, b) = cv {
-            let av = map[&a];
-            let bv = map[&b];
-            if a == M_HUMN || b == M_HUMN {
-                hv = Some(queue[1])
-            }
-            if a != M_HUMN && b != M_HUMN {
-                if let (N(na), N(nb)) = (&av, &bv) {
-                    if matches!(op, Op::Mul) && (na > &20 || nb > &20) {
-                        continue;
-                    }
-                    map.remove(&a);
-                    map.remove(&b);
-                    let nn = op.do_op(*na, *nb);
-                    map.insert(cm, N(nn));
-                    continue;
-                }
-            }
-            if hs.insert(cm) {
-                queue.push(cm);
-                if is_op!(av) {
-                    queue.push(a);
-                }
-                if is_op!(bv) {
-                    queue.push(b);
-                }
-            }
-        }
-    }
-    map.shrink_to_fit();
-    hv
-}
-
-fn resolve(map: &mut HashMap<Mn, OpType>, queue: &mut Vec<Mn>, start: Mn, rv: i64) -> Option<i64> {
-    use OpType::*;
-    queue.push(start);
-    let mut opv = vec![];
-    while let Some(cm) = queue.pop() {
-        let cv = map[&cm];
-        if let O(a, op, b) = cv {
-            let av = map[&a];
-            let bv = map[&b];
-            if a == M_HUMN || b == M_HUMN {
-                let N(on) = (if a == M_HUMN { bv } else { av }) else { panic!("Not optimized!") };
-                opv.push((op, on));
-            } else if opv.len() > 0 {
-                let on = match (av, bv) {
-                    (N(_), N(_)) | (O(_, _, _), O(_, _, _)) => panic!("Not optimized!"),
-                    (N(x), O(_, _, _)) => x,
-                    (O(_, _, _), N(x)) => x
-                };
-                opv.push((op, on));
-            } else {
-                queue.push(cm);
-                if is_op!(av) {
-                    queue.push(a);
-                }
-                if is_op!(bv) {
-                    queue.push(b);
-                }
-            }
-        }
-    }
-    let mut uhs = HashSet::new();
-    uhs.insert(rv);
-    while let Some((o, n)) = opv.pop() {
-        let cuv = uhs.drain().collect::<Vec<_>>();
-        for v in cuv {
-            match o {
-                Op::Div => {
-                    let mv = v * n;
-                    uhs.extend(mv..mv+n);
-                }
-                Op::Add => { uhs.insert(v - n); }
-                Op::Sub => { uhs.insert(v + n); }
-                Op::Mul => { uhs.insert(v / n); }
-            }
-        }
-    }
-    uhs.into_iter().min()
-    
-}
 
 pub fn part_2(input: &str) -> Option<i64> {
     use OpType::*;
-
-    let mut m = input.lines().map(parse_line).collect::<HashMap<_, _>>();
-    let mut q: Vec<Mn> = Vec::new();
-    let O(ma, _, mb) = m[&M_ROOT] else { return None; };
-    let ccom = optimize(&mut m, &mut q, M_ROOT).unwrap();
+    let mut mt = MTree::new(input);
+    let O(ma, _, mb) = mt.0[&M_ROOT] else { return None; };
+    let ccom = mt.optimize(M_ROOT).unwrap();
     let (calc, comp) = if ccom == ma { (mb, ma) } else { (ma, mb) };
-    let calcv = calculate(&mut m, &mut q, calc).unwrap();
+    let calcv = mt.calc(calc).unwrap();
     if cfg!(test) {
-        resolve(&mut m.clone(), &mut q, comp, calcv)
+        mt.resolve(comp, calcv)
     } else {
         let mut hn = 0;
         let mut dt = 1 << 26;
         loop {
-            *m.get_mut(&M_HUMN).unwrap() = N(hn);
-            let mut cm = m.clone();
-            let x = calculate(&mut cm, &mut q, comp).unwrap();
+            *mt.0.get_mut(&M_HUMN).unwrap() = N(hn);
+            let mut nmt = MTree (mt.0.clone());
+            let x = nmt.calc(comp).unwrap();
             if x == calcv {
                 loop {
-                    *m.get_mut(&M_HUMN).unwrap() = N(hn-1);
-                    let mut cm = m.clone();
-                    let x = calculate(&mut cm, &mut q, comp).unwrap();
+                    *mt.0.get_mut(&M_HUMN).unwrap() = N(hn-1);
+                    let mut nmt = MTree (mt.0.clone());
+                    let x = nmt.calc(comp).unwrap();
                     if x != calcv {
                         break;
                     }
